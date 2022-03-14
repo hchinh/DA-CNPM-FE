01ef855cdfccb7f596098156d1667770afe8adda
Chinh Huynh <chinh.huynh@team.enouvo.com>
2022-03-15 01:38:31 +0700

    config theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import 'antd/dist/antd.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
